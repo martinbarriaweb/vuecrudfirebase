f0694ee097fc87e47384c820323c51c64b51b1d4
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-21 19:19:55 -0600

    chore(app)clean

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsZyC3MtOnCzNuFntbua00hJvW92v8t7E",
+  authDomain: "vuecrud-d4ccb.firebaseapp.com",
+  databaseURL: "https://vuecrud-d4ccb.firebaseio.com",
+  projectId: "vuecrud-d4ccb",
+  storageBucket: "vuecrud-d4ccb.appspot.com",
+  messagingSenderId: "554387118339",
+  appId: "1:554387118339:web:bc97fac641d529a94ef136",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
