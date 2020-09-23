007f49f24a61148dc2e0961d68266ae13beda1d3
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-22 18:30:21 -0600

    fix(edit): task

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { db } from "../firebase";
+import router from "../router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     tasks: [],
+    task: {
+      name: "",
+      id: "",
+    },
   },
   mutations: {
     setTasks(state, payload) {
       state.tasks = payload;
+    },
+    setTask(state, payload) {
+      state.task = payload;
     },
   },
   actions: {
@@ -25,6 +33,27 @@ export default new Vuex.Store({
             tasks.push(task);
           });
           commit("setTasks", tasks);
+        });
+    },
+    getTask({ commit }, idTask) {
+      db.collection("tarea")
+        .doc(idTask)
+        .get()
+        .then((doc) => {
+          let tarea = doc.data();
+          tarea.id = doc.id;
+          commit("setTask", tarea);
+          console.log(tarea);
+        });
+    },
+    editTask({ commit }, task) {
+      db.collection("tarea")
+        .doc(task.id)
+        .update({
+          nombre: task.nombre,
+        })
+        .then(() => {
+          router.push("/");
         });
     },
   },
--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -1,13 +1,29 @@
 <template>
-  <h1>{{id}}</h1>
+  <div>
+    <h1>{{task.id}} - {{task.nombre}}</h1>
+    <form @submit.prevent="editTask(task)">
+      <input v-model="task.nombre" type="text" />
+      <button type="submit">Editar</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 export default {
   data() {
     return {
       id: this.$route.params.id,
     };
+  },
+  created() {
+    this.getTask(this.id);
+  },
+  methods: {
+    ...mapActions(["getTask", "editTask"]),
+  },
+  computed: {
+    ...mapState(["task"]),
   },
 };
 </script>
